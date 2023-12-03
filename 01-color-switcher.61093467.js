1582d8a5eac78ef627c326daf901c3f1bfdd852f
AnnaHrynova <137887642+AnnaHrynova@users.noreply.github.com>
2023-12-03 11:02:37 +0000

    Deploying to gh-pages from @ AnnaHrynova/goit-js-hw-09@9e989cca93840f49f4bcc3ff3940cb142615f822 🚀

--- 01-color-switcher.61093467.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.body;let d;t.addEventListener("click",(()=>{t.disabled=!0,d=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;a.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(()=>{t.disabled=!1,setTimeout((()=>clearInterval(d)))}));
-//# sourceMappingURL=01-color-switcher.61093467.js.map
---
+++ 01-color-switcher.746f1814.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.body;let a;t.disabled=!0;e.addEventListener("click",(()=>{e.disabled=!0,t.disabled=!1,a=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;d.style.backgroundColor=e}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,setTimeout((()=>clearInterval(a)))}));
+//# sourceMappingURL=01-color-switcher.746f1814.js.map
